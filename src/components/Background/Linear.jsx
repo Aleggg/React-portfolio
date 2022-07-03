3674ebd28492b300385cfc746211b3a00c454303
Oleh Hasii <olehhasii@gmail.com>
2022-07-03 23:25:11 +0300

    Finished 3d background

--- src/components/Background/Linear.jsx
+++ src/components/Background/Linear.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import { Canvas, useLoader, useFrame } from '@react-three/fiber';
 import { TextureLoader } from 'three';
 
-const TEXTURE_PATH =
-	'https://res.cloudinary.com/dg5nsedzw/image/upload/v1641657168/blog/vaporwave-threejs-textures/grid.png';
-
 const Plane = React.forwardRef((props, ref) => {
 	const z = props.z;
+	const y = props.y;
+	const x = props.x;
+	const rotationX = props.rotationX;
 	const textureMap = useLoader(TextureLoader, 'grid.png');
 	return (
-		<mesh ref={ref} rotation={[-Math.PI * 0.5, 0, 0]} position={[0, 0, z]}>
+		<mesh ref={ref} rotation={[rotationX, 0, 0]} position={[x, y, z]}>
 			<planeBufferGeometry arrach='geometry' args={[1, 2, 24, 24]} />
 			<meshStandardMaterial map={textureMap} />
 		</mesh>
@@ -17,12 +17,6 @@ const Plane = React.forwardRef((props, ref) => {
 });
 
 const Light = () => {
-	const spotlight1Ref = React.useRef();
-	const spotlight2Ref = React.useRef();
-
-	spotlight1Ref.current?.target.position.set([-0.25, 0.25, 0.25]);
-	spotlight2Ref.current?.target.position.set([0.25, 0.25, 0.25]);
-
 	return (
 		<>
 			<ambientLight intensity={5} />
@@ -30,22 +24,121 @@ const Light = () => {
 	);
 };
 const Landscape = () => {
-	const terrain1Ref = React.useRef();
-	const terrain2Ref = React.useRef();
+	const speed = 0.1;
+
+	const planeRef1 = React.useRef();
+	const planeRef2 = React.useRef();
+	const planeRightRef1 = React.useRef();
+	const planeRightRef2 = React.useRef();
+	const planeRefTop1 = React.useRef();
+	const planeRefTop2 = React.useRef();
+	const planeTopRightRef1 = React.useRef();
+	const planeTopRightRef2 = React.useRef();
+	const planeLeftRef1 = React.useRef();
+	const planeLeftRef2 = React.useRef();
+	const planeTopLeftRef1 = React.useRef();
+	const planeTopLeftRef2 = React.useRef();
 
 	useFrame((state) => {
 		// Update plane position
-		terrain1Ref.current.position.z = (state.clock.elapsedTime * 0.15) % 2;
-		terrain2Ref.current.position.z = ((state.clock.elapsedTime * 0.15) % 2) - 2;
+		planeRef1.current.position.z = (state.clock.elapsedTime * speed) % 2;
+		planeRef2.current.position.z = ((state.clock.elapsedTime * speed) % 2) - 2;
+		planeRefTop1.current.position.z = (state.clock.elapsedTime * speed) % 2;
+		planeRefTop2.current.position.z =
+			((state.clock.elapsedTime * speed) % 2) - 2;
+		planeRightRef1.current.position.z = (state.clock.elapsedTime * speed) % 2;
+		planeRightRef2.current.position.z =
+			((state.clock.elapsedTime * speed) % 2) - 2;
+		planeTopRightRef1.current.position.z =
+			(state.clock.elapsedTime * speed) % 2;
+		planeTopRightRef2.current.position.z =
+			((state.clock.elapsedTime * speed) % 2) - 2;
+		planeLeftRef1.current.position.z = (state.clock.elapsedTime * speed) % 2;
+		planeLeftRef2.current.position.z =
+			((state.clock.elapsedTime * speed) % 2) - 2;
+		planeTopLeftRef1.current.position.z = (state.clock.elapsedTime * speed) % 2;
+		planeTopLeftRef2.current.position.z =
+			((state.clock.elapsedTime * speed) % 2) - 2;
 	});
 
 	return (
 		<>
-			<Plane ref={terrain1Ref} z={0.15} />
-			<Plane ref={terrain2Ref} z={-2} />
+			<Plane ref={planeRef1} z={0.15} rotationX={-Math.PI * 0.5} y={0} x={0} />
+			<Plane ref={planeRef2} z={-1.85} rotationX={-Math.PI * 0.5} y={0} x={0} />
+			<Plane
+				ref={planeRefTop1}
+				z={0.15}
+				rotationX={Math.PI * 0.5}
+				y={0.2}
+				x={0}
+			/>
+			<Plane
+				ref={planeRefTop2}
+				z={-1.85}
+				rotationX={Math.PI * 0.5}
+				y={0.2}
+				x={0}
+			/>
+			<Plane
+				ref={planeRightRef1}
+				z={0.15}
+				rotationX={-Math.PI * 0.5}
+				y={0}
+				x={1}
+			/>
+			<Plane
+				ref={planeRightRef2}
+				z={-1.85}
+				rotationX={-Math.PI * 0.5}
+				y={0}
+				x={1}
+			/>
+			<Plane
+				ref={planeTopRightRef1}
+				z={0.15}
+				rotationX={Math.PI * 0.5}
+				y={0.2}
+				x={1}
+			/>
+			<Plane
+				ref={planeTopRightRef2}
+				z={-1.85}
+				rotationX={Math.PI * 0.5}
+				y={0.2}
+				x={1}
+			/>
+			<Plane
+				ref={planeLeftRef1}
+				z={0.15}
+				rotationX={-Math.PI * 0.5}
+				y={0}
+				x={-1}
+			/>
+			<Plane
+				ref={planeLeftRef2}
+				z={-1.85}
+				rotationX={-Math.PI * 0.5}
+				y={0}
+				x={-1}
+			/>
+			<Plane
+				ref={planeTopLeftRef1}
+				z={0.15}
+				rotationX={Math.PI * 0.5}
+				y={0.2}
+				x={-1}
+			/>
+			<Plane
+				ref={planeTopLeftRef2}
+				z={-1.85}
+				rotationX={Math.PI * 0.5}
+				y={0.2}
+				x={-1}
+			/>
 		</>
 	);
 };
+
 const Linear = () => {
 	return (
 		<Canvas
